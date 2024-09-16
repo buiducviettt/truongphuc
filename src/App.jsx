cc722cd1b128ef153d2f851929e445868ad16f49
buiducviettt <ducviet8a4@gmail.com>
2024-09-16 23:17:23 +0700

    set up tiktok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 
-import './App.css'
-
+import{BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
+  return (
+    <Router>
+      <h1>Hello World</h1>
+    </Router>
+  )
   
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import GlobalStyle from './components/GlobalStyles/index.jsx'
 
 createRoot(document.getElementById('root')).render(
+ 
   <StrictMode>
-    <App />
+     <GlobalStyle>
+      <App />
+      </GlobalStyle>
   </StrictMode>,
 )
