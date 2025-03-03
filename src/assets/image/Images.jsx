fc65db2aa5017a233856b8a93d032ba8bf564de0
buiducviettt <ducviet8a4@gmail.com>
2025-03-03 18:01:42 +0700

    hoan thien

--- src/assets/image/Images.jsx
+++ src/assets/image/Images.jsx
@@ -1,10 +1,20 @@
 import logo from '../image/logo/logo.svg';
 import arrow from '../image/icons/arrow.svg';
 import bannerhome from '../image/banner/homebanner.png';
+import thumb3 from '../image/banner/thumb3.jpg';
+import thumb4 from '../image/banner/thumb4.jpg';
+import thumb5 from '../image/banner/thumb5.jpg';
 import homeintro from '../image/homepage/homeintro.svg';
 import gallery1 from '../image/homepage/gallery1.png';
+import subtract2 from '../image/homepage/subtractyellow.svg';
+import subtract from '../image/homepage/subtractblue.svg';
 
 const Images = {
+  thumb4,
+  thumb5,
+  thumb3,
+  subtract,
+  subtract2,
   gallery1,
   homeintro,
   bannerhome,
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,12 +5,29 @@ import BlurText from '../../components/BlurText';
 import '../components/styles/home.scss';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import Images from '../../assets/image/Images';
+import CountUp from 'react-countup';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectFade } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-fade';
 
+import Images from '../../assets/image/Images';
+import { useInView } from 'react-intersection-observer';
 import { getDataHome } from '../../assets/api/api';
 import Button from '../../components/Button';
 const Home = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.6,
+  });
+  const thumbnail = [
+    Images.bannerhome,
+    Images.thumb3,
+    Images.thumb4,
+    Images.thumb5,
+  ];
   const [dataHome, setDataHome] = useState(null);
+  const [mainImage, setMainImage] = useState(thumbnail[0]);
   useEffect(() => {
     const fetchData = async () => {
       const data = await getDataHome();
@@ -27,7 +44,31 @@ const Home = () => {
       once: true, // Chỉ chạy một lần khi scroll xuống
     });
   }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMainImage((prevImage) => {
+        const currentIndex = thumbnail.indexOf(prevImage);
+        const nextIndex = (currentIndex + 1) % thumbnail.length;
+        return thumbnail[nextIndex];
+      });
+    }, 4000);
+
+    return () => clearInterval(interval);
+  }, [thumbnail]);
+
   if (!dataHome) return <p>Loading...</p>;
+  // các câu lệnh phải thực hiện sau dòng !dataHome này
+  const highlight_number = [
+    {
+      number: dataHome.special_number.highlight_number_1,
+      content: dataHome.special_number.content_1,
+    },
+    {
+      number: dataHome.special_number.highlight_number_2,
+      content: dataHome.special_number.content_2,
+    },
+  ];
+
   return (
     <DefaultLayout>
       <div className="homepage">
@@ -36,7 +77,7 @@ const Home = () => {
             <div
               className="home_banner"
               style={{
-                backgroundImage: `url('${dataHome.banner_section.image}')`, // Đặt ảnh nền cho banner
+                backgroundImage: `url(${mainImage})`, // Đặt ảnh nền cho banner
               }}
             >
               <div className="banner_overlay"></div>
@@ -60,49 +101,82 @@ const Home = () => {
                   />
                 </div>
               </div>
+
               <div className="banner_gallery">
-                <div className="img_wrapper">
-                  <img
-                    src={Images.gallery1}
-                    alt=""
-                    className="banner_gallery_img"
-                  />
-                </div>
-                <div className="img_wrapper">
-                  <img
-                    src={Images.gallery1}
-                    alt=""
-                    className="banner_gallery_img"
-                  />
-                </div>
-                <div className="img_wrapper">
-                  <img
-                    src={Images.gallery1}
-                    alt=""
-                    className="banner_gallery_img"
-                  />
-                </div>
-                <div className="img_wrapper">
-                  <img
-                    src={Images.gallery1}
-                    alt=""
-                    className="banner_gallery_img"
-                  />
-                </div>
+                {thumbnail.map((img, index) => (
+                  <div className="img_wrapper" key={index}>
+                    <img
+                      src={img}
+                      alt=""
+                      className="thumbnail_img"
+                      onClick={() => setMainImage(img)}
+                    />
+                  </div>
+                ))}
               </div>
             </div>
             <section className="home_intro">
-              <div className="home_intro_wrapper">
+              <div className="substract_wrapper">
+                <div className="subtract">
+                  <img src={Images.subtract} alt="" className="subtract_img" />
+                </div>
+                <div className="subtract2">
+                  <img src={Images.subtract2} alt="" className="subtract_img" />
+                </div>
+              </div>
+              <div className="home_intro_wrapper ">
                 <div className="container">
                   <div className="home_intro_inner">
-                    <h2 className="text_title text_intro ">Hewllo</h2>
+                    <div className="intro_content" data-aos="fade-up">
+                      <div className="intro_title">
+                        <p className="text-white text_title text_intro ">
+                          {dataHome.home_desc.home_desc_title}
+                        </p>
+                      </div>
+                      <div className="intro_desc">
+                        <div className="right_content ">
+                          <p className="text-white">
+                            {dataHome.home_desc.home_desc_detail}
+                          </p>
+                          <div className="box_numbers">
+                            {highlight_number.map((number) => (
+                              <div key={number.id} className="box_number">
+                                <p
+                                  ref={ref}
+                                  className={`text-white number_detail `}
+                                >
+                                  {inView && (
+                                    <CountUp
+                                      start={0}
+                                      end={
+                                        number.number === '0'
+                                          ? '24/7'
+                                          : parseFloat(number.number)
+                                      }
+                                      duration={3}
+                                      separator=","
+                                      decimalPlaces={2}
+                                    />
+                                  )}
+                                </p>
+                                <p className="text-white">{number.content}</p>
+                              </div>
+                            ))}
+                            <div className="24/7_content">
+                              <p className="box_number number_detail">24/7</p>
+                              <p className="text-white">
+                                {dataHome.special_number.content_3}
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </section>
-            <section>
-              <h1>dakjahskjd</h1>
-            </section>
+            <section></section>
           </div>
         </div>
       </div>
